509708987d95d6c1acc1fe996151e793e0c9a240
thedocbwarren <jedi.master@augmentedjs.com>
2024-11-11 12:24:51 -0800

    rainbow

--- bigletters.h
+++ bigletters.h
@@ -5,8 +5,8 @@
 #define MAX_HEIGHT 6
 #define MAX_CHARS 20
 
-// ANSI color codes for CLI colors (16 colors)
-extern const char *colors[16];
+// ANSI color codes for CLI colors
+extern const char *colors[];
 
 // Extern declarations for normal and bold fonts
 extern const char *letters_normal[26][MAX_HEIGHT];
@@ -15,9 +15,9 @@ extern const char *letters_bold[26][MAX_HEIGHT];
 extern const char *numbers_bold[10][MAX_HEIGHT];
 
 // Function declarations
-void print_large_text(const char *text, int color_code, int bold);
-int parse_arguments(int argc, char *argv[], char *text, int *color, int *bold);
-int validate_input(const char *text, int color_code);
+void print_large_text(const char *text, int color_code, int bold, int rainbow);
+int parse_arguments(int argc, char *argv[], char *text, int *color, int *bold, int *rainbow);
+int validate_input(const char *text, int color_code, int rainbow);
 void display_usage();
 
 #endif // BIGLETTERS_H
